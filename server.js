fe78ca05a2a98b575b34c1847eea0cf8132a10a6
Wojciech Kalebasiak <kalebasiakw@gmail.com>
2018-06-07 17:43:40 +0200

    Connect to mlab database

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase');
+mongoose.connect('mongodb://wojtaaz:gitgit1@ds247290.mlab.com:47290/mynodeappdatabase');
 
 const userSchema = new Schema({
     name: String,
